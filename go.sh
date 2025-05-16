16a33cba51cd5d96a19c08ac0d14f71daf8637d1
necarlson97 <nilscarlson1997@gmail.com>
2025-05-16 16:38:09 -0400

    Add bash script to get utf-8 font on raspbian

---
+++ go.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Set a fallback font config if fbterm can't pick up the system one
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
+
+# Launch fbterm with a nice monospace font
+# You can use 'NotoMono', 'DejaVu Sans Mono', etc. (run `fc-list :mono` to check installed monospace fonts)
+
+FONT="Noto Mono"
+
+fbterm -s 20 -- bash -c "python3 /home/pi/Terminal-Boids/main.py"
